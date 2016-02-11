dfe0e25fbbb8f27c2410957419712fc6965783de
karthikbaputj <karthikbaputj@gmail.com>
2016-02-11 15:57:11 -0500

    Update iOSPageObjectTest.java

--- java/junit/src/test/java/concur/iOSPageObjectTest.java
+++ java/junit/src/test/java/concur/iOSPageObjectTest.java
@@ -43,7 +43,7 @@ public class iOSPageObjectTest {
 
         driver.findElement(By.name("User name")).clear();
         driver.findElement(By.name("User name")).sendKeys("autoadmin@gsa-automation.gov");
-        driver.findElement(By.name("Password or Pin")).sendKeys("outtask1");
+        driver.findElement(By.name("Password or Pin")).sendKeys("sdfds");
         driver.findElement(By.name("Done")).click();
         driver.findElement(By.name("OK")).click();
         Assert.assertEquals("Planning your trip screen", "Start by planning your trip", driver.findElement(By.name("Start by planning your trip")).getText());
